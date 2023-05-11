0a7ad94fb83bf06b7e3d825d0b9e9808023f83a1
kristian <kfskibrek@icloud.com>
2023-05-11 15:06:10 +0200

    more columns for subway

--- textCreation/generateImages.sh
+++ textCreation/generateImages.sh
@@ -4,7 +4,7 @@ touch images
 echo [ >> images
 for d in ./../images/*; do
  echo $d
- python3 main.py --file $d --cols 200
+ python3 main.py --file $d --cols 400
  echo '`' >> images
  cat ./out.txt >> images
  echo '`,' >> images
