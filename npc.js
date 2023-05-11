c038076cdc44d6da45a07a138ab0ca766fd8cd34
kristian <kfskibrek@icloud.com>
2023-05-11 17:34:18 +0200

    choose animation through command line arguments

--- npc.js
+++ npc.js
@@ -1,9 +1,22 @@
-import {frames} from './textCreation/imageTexts.js'
+//import {frames} from './textCreation/imageTexts.js'
+var frames
+if(process.argv[2]){
+  import('./animations/' + process.argv[2] + '.js').then(animation => {
+    animate(animation.frames)
+  })
+}
+else{
+  import('./textCreation/imageTexts.js').then(animation => {
+    animate(animation.frames)
+  })
+}
 
-var frame = 0
-setInterval(() => {
-    console.log('Venaas er NPC')
-    console.log(frames[frame])
-    frame == frames.length - 1 ? frame = 0 : frame ++
-}, 200);
+function animate(frames){
+  var frame = 0
+  setInterval(() => {
+      console.log('Venaas er NPC')
+      console.log(frames[frame])
+      frame == frames.length - 1 ? frame = 0 : frame ++
+  }, 200);
+}
 
