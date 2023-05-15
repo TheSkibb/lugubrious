1332a98a64d8dc3565f11c3c54264a06d0e48ffd
kristian <kfskibrek@icloud.com>
2023-05-15 16:10:20 +0200

    remove comment

--- npc.js
+++ npc.js
@@ -20,7 +20,6 @@ else{
 function animate(frames){
   var frame = 0
   setInterval(() => {
-      console.log('Venaas er NPC')
       console.log(frames[frame])
       frame == frames.length - 1 ? frame = 0 : frame ++
   }, 200);
